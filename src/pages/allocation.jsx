3ad1a04654b79627cdaca2986b3d7c7d6954e333
luckykidv <mwangivictor770@gmail.com>
2026-03-05 12:41:00 +0300

    feat: implemented login and sign up pages

---
+++ src/pages/allocation.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+
+import './styles/allocation.css'
+
+function Allocation() {
+  return(
+    <>
+    
+    </>
+  )
+}
+
+export default Allocation
